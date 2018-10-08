28bfcc2ba3604103d6ac1b86644cf31f6f5d4f3c
Nyjel <ndukescmg@live.com>
2018-10-08 15:58:43 -0400

    Starting work on the second page

--- app/src/main/java/org/computermentors/interactivestorymon/MainActivity.java
+++ app/src/main/java/org/computermentors/interactivestorymon/MainActivity.java
@@ -2,12 +2,30 @@ package org.computermentors.interactivestorymon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nameEditText;
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameEditText = findViewById(R.id.nameEditText);
+        startButton = findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = nameEditText.getText().toString().trim();
+                Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
